cfaa00482122a1d02d331512628d3d7a4b001c3e
Hrushikesh <shreeman2003dkl@gmail.com>
2024-10-23 20:16:10 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const PORT = 4000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello js');
+});
+
+app.get('/users',(req,res)=>{
+    res.status(200).json({
+        message:"working"
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
